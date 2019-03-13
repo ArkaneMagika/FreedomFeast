80beed4328d7aa869041622510e4f36dedfacf72
Thomas <esemplaret1@gmail.com>
2019-03-13 14:47:25 -0400

    Started the UI

--- ff-access/src/app/app.component.ts
+++ ff-access/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ff-access';
+  title = 'whatever the UI is gonna look like';
 }
