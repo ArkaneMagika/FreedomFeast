437c8eee3f8426d8339ae1a8ab834c56f8a23527
Akane <akane.simpson@gmail.com>
2019-03-18 15:11:54 -0400

    Login and Provider controller created

--- controllers/Login.controller.js
+++ controllers/Login.controller.js
@@ -1,17 +1,43 @@
-class LoginController{
-    checkPassword(pass, user_pass){
-        bcrypt.compare(pass, user_pass, function(err, passCheck){
-    
-            if(passCheck){
-                const token = jwt.sign(user, process.env.JWT_SECRET,{
-                    expiresIn:60*2
+const User = require('../models/User')
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt')
+
+class LoginController {
+
+    checkUser(username) {
+        UserQuery = User.where({ username: username })
+        UserQuery.findOne(function (err, user_search) {
+
+            for (user of user_search) {
+
+                if (user.username == username) {
+                    token = checkPassword(password, user.password)
+                    res.send({
+                        token
+                    })
+                }
+                else {
+                    console.error(`${err}`)
+                }
+            }
+        })
+    }
+
+    checkPassword(pass, user_pass) {
+        bcrypt.compare(pass, user_pass, function (err, passCheck) {
+
+            if (passCheck) {
+                const token = jwt.sign(user, process.env.JWT_SECRET, {
+                    expiresIn: 60 * 2
                 });
                 return token;
             }
-            else{
+            else {
                 console.error(`Passwords do no match: ${err}`)
                 return err;
             }
         })
     }
 }
+
+exports.LoginController = LoginController
